813fa4825be3d5de23d3b93cd1a2f37d20db946b
Mankaran Rooprai <mankaranrooprai@gmail.com>
2022-08-18 15:33:44 -0400

    setup user input and server

--- email-sender.py
+++ email-sender.py
@@ -1,1 +1,13 @@
-#hello
+import smtplib
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+
+email = input("enter your email: ")
+password = input("enter your password: ")
+receiver = input("what email do you want to send to: ")
+
+server.login(email, password)
+
+server.sendmail(email, receiver, 'Hello World!')
+
+print('mail sent!')
